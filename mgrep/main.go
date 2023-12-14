20d8d9775c6cfb964dcaf0c63ff90310240f6064
experiencei <ayelojahighbee01@gmail.com>
2023-12-14 19:40:04 +0100

    working on mgrep project for searching text in directories

--- mgrep/main.go
+++ mgrep/main.go
@@ -26,3 +26,74 @@ func discoverDirs(wl *worklist.Worklist, path string) {
 		}
 	}
 }
+
+var args struct {
+	SearchTerm string `arg:"positional,required"`
+	SearchDir  string `arg:"positional"`
+}
+
+func main() {
+	arg.MustParse(&args)
+
+	var workersWg sync.WaitGroup
+
+	wl := worklist.New(100)
+
+	results := make(chan worker.Result, 100)
+
+	numWorkers := 10
+
+	workersWg.Add(1)
+	go func() {
+		defer workersWg.Done()
+		discoverDirs(&wl, args.SearchDir)
+		wl.Finalize(numWorkers)
+	}()
+
+	for i := 0; i < numWorkers; i++ {
+		workersWg.Add(1)
+		go func() {
+			defer workersWg.Done()
+			for {
+				workEntry := wl.Next()
+				if workEntry.Path != "" {
+					workerResult := worker.FindInFile(workEntry.Path, args.SearchTerm)
+					if workerResult != nil {
+						for _, r := range workerResult.Inner {
+							results <- r
+						}
+					}
+				} else {
+					// When the path is empty, this indicates that there are no more jobs available,
+					// so we quit.
+					return
+				}
+			}
+		}()
+	}
+
+	blockWorkersWg := make(chan struct{})
+	go func() {
+		workersWg.Wait()
+		// Close channel
+		close(blockWorkersWg)
+	}()
+
+	var displayWg sync.WaitGroup
+
+	displayWg.Add(1)
+	go func() {
+		for {
+			select {
+			case r := <-results:
+				fmt.Printf("%v[%v]:%v\n", r.Path, r.LineNum, r.Line)
+			case <-blockWorkersWg:
+				// Make sure channel is empty before aborting display goroutine
+				if len(results) == 0 {
+					displayWg.Done()
+					return
+				}
+			}
+		}
+	}()
+}
