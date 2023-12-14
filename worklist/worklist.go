a519e84613cfe62669a841752b90338fea1699c2
experiencei <ayelojahighbee01@gmail.com>
2023-12-14 19:36:48 +0100

    working on mgrep project for searching text in directories

---
+++ worklist/worklist.go
@@ -0,0 +1,27 @@
+package worklist
+
+// Empty path indicates that there are no more jobs to be done
+type Entry struct {
+	Path string
+}
+
+type Worklist struct {
+	jobs chan Entry
+}
+
+func (w *Worklist) Add(work Entry) {
+	w.jobs <- work
+}
+
+func (w *Worklist) Next() Entry {
+	j := <-w.jobs
+	return j
+}
+
+func New(bufSize int) Worklist {
+	return Worklist{make(chan Entry, bufSize)}
+}
+
+func NewJob(path string) Entry {
+	return Entry{path}
+}
